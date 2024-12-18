94d4eb019faa75e8fb77696ecfaee37cf1de5ae1
Ana Leventić <ana.leventic@b2match.com>
2024-12-18 23:38:02 +0100

    refactor

--- app/main.go
+++ app/main.go
@@ -24,46 +24,68 @@ type Price struct {
 	Currency           string  `json:"currency"`
 }
 
-func main() {
+type Application struct {
+	DB *sql.DB
+}
+
+func setUpApp() (Application, error) {
+	var application Application
 	// Connect to MySQL
 	dsn := "root:password@tcp(db:3306)/mytheresa"
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		log.Fatal("Database connection failed:", err)
+		return application, fmt.Errorf("database connection failed: %v", err)
 	}
-	defer db.Close()
 
-	// Handle products endpoint
-	http.HandleFunc("/products", func(w http.ResponseWriter, r *http.Request) {
-		rows, err := db.Query("SELECT sku, name, category, price FROM products LIMIT 5")
-		if err != nil {
-			http.Error(w, "Failed to fetch products", http.StatusInternalServerError)
+	application.DB = db
+
+	return application, nil
+}
+
+func (application *Application) productsHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Products Handler starting...")
+	defer log.Printf("Products Handler finished.")
+
+	rows, err := application.DB.Query("SELECT sku, name, category, price FROM products LIMIT 5")
+	if err != nil {
+		http.Error(w, "Failed to fetch products", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var products []Product
+	for rows.Next() {
+		var p Product
+		if err := rows.Scan(&p.SKU, &p.Name, &p.Category, &p.Price.Original); err != nil {
+			http.Error(w, "Error scanning product", http.StatusInternalServerError)
 			return
 		}
-		defer rows.Close()
+		products = append(products, p)
+	}
 
-		var products []Product
-		for rows.Next() {
-			var p Product
-			if err := rows.Scan(&p.SKU, &p.Name, &p.Category, &p.Price.Original); err != nil {
-				http.Error(w, "Error scanning product", http.StatusInternalServerError)
-				return
-			}
-			products = append(products, p)
-		}
+	log.Println("Products fetched successfully")
 
-		log.Println("Products fetched successfully")
-		w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 
-		// Marshal the products slice into pretty JSON format
-		formattedJSON, err := json.MarshalIndent(products, "", "    ")
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error marshalling JSON: %v", err), http.StatusInternalServerError)
-			return
-		}
+	// Marshal the products slice into pretty JSON format
+	formattedJSON, err := json.MarshalIndent(products, "", "    ")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error marshalling JSON: %v", err), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(formattedJSON)
+}
 
-		w.Write(formattedJSON)
-	})
+func main() {
+	application, err := setUpApp()
+	if err != nil {
+		log.Fatal("Failed to set up application:", err)
+	}
+	defer application.DB.Close()
+
+	// Register handlers
+	http.HandleFunc("/products", application.productsHandler)
 
 	// Start server
 	log.Println("Server is running on port 8080")
